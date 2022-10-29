562dfd212062465ec56b689a630611564f4095a3
demaalanezi <115086419+demaalanezi@users.noreply.github.com>
2022-10-29 19:57:52 +0300

    Done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+function BMI(weight,height) {
+    let result = weight / (height * height);
+    return result;
+}
+console.log(BMI(80,1.8));
+
+function Status(BodyMass){
+    if(BodyMass<18.5){
+        return "لديك نقص في الوزن";
+    } else if(BodyMass <25 && BodyMass> 18.5){
+        return "وزنك صحي";
+    } else if(BodyMass > 25){
+        return "لديك زيادة في الوزن";
+    }
+}
+
+function calcultor(){
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+    let bmi_value = BMI(weight,height);
+    let desc = Status(bmi_value);
+
+    let box = document.getElementById("result");
+    box.innerText = bmi_value + " == " + desc;
+}
